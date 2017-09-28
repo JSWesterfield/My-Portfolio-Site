9b4d641b4dcaec9457433dada2766acc08575f49
Jake <jecobwesterfield@gmail.com>
2017-09-28 01:27:45 -0700

    added JS modules
    added javascript modules from js folder hopefully will compile and execute

--- server.js
+++ server.js
@@ -1,4 +1,27 @@
+//adding this node.js express 'module', using the require function/method
 var express = require('express');
+
+//javascript libraries for portfolio page
+var bootstrap = require('js/bootstrap.min.js');
+var custom = require('js/custom.js');
+var imagesLoaded = require('js/imagesloaded.min.js');
+var isotope = require('js/isotope.js');
+var isotopeMin = require('js/isotope.min.js');
+var jqueryBackstretch = require('js/jquery.backstretch.min.js');
+var jqueryFlex = require('js/jquery.flexslider-min.js');
+var jquery = require('js/jquery.js');
+var jqueryMagnific = require('js/jquery.magnific-popup.min.js');
+var jqueryParallax = require('js/jquery.parallax.js');
+var jquerySticky = require('js/jquery.sticky.js');
+var nivoLightbox = require('js/nivo-lightbox.min.js');
+var smoothScroll = require('js/smoothscroll.js');
+var wow = require('js/wow.min.js');
+var jake = require('js/jake.js');
+var services = require('js/services/jake.services.form.js'); //Do I initialize a service here? or somewhere else? 
+
+
+
+//declare and initialize an 'app' variable to use the new 'express' method.
 var app = express();
 
 // set the port of our application
@@ -18,8 +41,8 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
 
     // ejs render automatically looks in the views folder
-    //res.render('index');
-    res.render('index2');
+    //res.render('index'); //alternative view, my initial view to test out the express server
+    res.render('index2'); //actual portfolio page I want to render
 });
 
 app.listen(port, function() {
