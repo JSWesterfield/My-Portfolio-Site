4b838a1394dd4ad1c19fdf1856da244663ea5b35
Jake <jecobwesterfield@gmail.com>
2018-01-17 02:49:15 -0700

    added new ajax call from a different weather API, I choose darkSky API because it meshes well with SkyCons icon codes, and of course DarkSky API actually created the SkyCons icons, in addition to this I wouldnt have to create an object to map the SkyCons icons to the openweathermap API icon codes, this would be no problem for one ajax call but having to render several cities Im only guessing the complexity of the code will increase and simplicity with render the weather data will be one less thing to think about.

--- public/js/config/config.js
+++ public/js/config/config.js
@@ -1,4 +1,8 @@
 var config = {
-    OWA_KEY: "d8be0f02752ec0698888d899e60425d8",
-    Flickr_APIKey: "dasdasd2213"
+    //Open Weather Map API key
+    owm_KEY: "d8be0f02752ec0698888d899e60425d8",
+    //Flickr API key
+    flickr_APIKey: "dasdasd2213",
+    //Dark Sky API key
+    darkSky: "845ff6b003aba8bd1a9434ab040ef79c"
 }
--- public/js/weatherly.js
+++ public/js/weatherly.js
@@ -1,4 +1,4 @@
-    
+
     var weatherData = {
       city: document.querySelector ("#userCity"),
       state: document.querySelector ("#userState"), 
@@ -25,63 +25,215 @@
       }
         weatherData.temperature.innerHTML = weatherData.temperatureValue + weatherData.units + " ";
     }
-
+    //https://api.darksky.net/forecast/845ff6b003aba8bd1a9434ab040ef79c/37.8267,-122.4233
     // get geolocation data from ip via ip-api.com
     var getIP = 'http://ip-api.com/json/';
-
+    //Dark Sky API key: 845ff6b003aba8bd1a9434ab040ef79c
+    //OpenWeatherMap API key: 
     // get weather data from Open Weather API, use geolocation 
-    var openWeatherMap = 'http://api.openweathermap.org/data/2.5/weather'
+    // var openWeatherMap = 'http://api.openweathermap.org/data/2.5/weather'
+    // var darkSkyAPI = 'https://api.darksky.net/forecast/'
+    // var CORS = 'https://cors-anywhere.herokuapp.com/'
+    // var apiKey = '845ff6b003aba8bd1a9434ab040ef79c/'
     $.getJSON(getIP).done(function(location) {
-        if(!location) {
-          //error, couldn't find location, execute sweetalert 'error' function
-          swal("Oops!", "Something went wrong on the page!", "error");
-        }
-        else { 
         //show location object in console, to pull out 'regionName' or the state for rendering in html
         console.log(location);
         console.log("Your region/state name is: " + location.regionName);
           var regionName = location.regionName;
           weatherData.state.innerHTML = regionName + ", ";
-        } 
-        $.getJSON(openWeatherMap, {
-            lat: location.lat,
-            lon: location.lon,
-            units: 'metric',
-            APPID: 'd8be0f02752ec0698888d899e60425d8'
-        }).done(function(response) {
-              console.log(response);
-              var position = {
-                latitude: response.coord.lat,
-                longitude: response.coord.lon
-              };
-              var cityName = response.name;
+          //Now that we have the location data we can pass this within our DarkSky Ajax call
+          var darkSkyURL = 'https://api.darksky.net/forecast/'
+          // To bypass the Cross Domain Error, can I pass in an object of mode: 'no-cors' as my proxy variable?
+          // or is this only used with the fetch function? 
+          // the use of:      
+          //        mode: 'no-cors' to get rid of the CORS issue: 
+          //    EXAMPLE:
+          //        fetch(apiLinkDS, { 
+          //              mode: 'no-cors' 
+          //           })
+          //
+          var corsProxy = 'https://cors-anywhere.herokuapp.com/' //can I pass in a mode: 'no-cors' object in place of this proxy?
+          var apiKey = '845ff6b003aba8bd1a9434ab040ef79c/' 
+          // will locationData be a string? location.lat object + string + location.lat object == String??  
+          // OR is locationData = (response object + string object + response object) ??
+          var locationData = location.lat + "," + location.lon; //should I use ( ) to wrap these 3 objects together?
+          var requestObject = corsProxy + darkSkyURL + apiKey + locationData;
+          //start Ajax call pass in the above data, in order:
+          // CORS + DarkSky API URL + API KEY + locationData
+          $.getJSON(requestObject).done(function(response) {
+                console.log(response);
+                //why do I need this positon object? 
+                var position = {
+                  latitude: response.latitude,
+                  longitude: response.longitude
+                };
+                var cityName = location.city;
 
-              //var weatherSimpleDescription = response.weather.simple;
-              var weatherDescription = response.weather.description;
-              //set our initial temperature to Fahrenheit due to the response data return the unit of celsius as the response data
-              var weatherTemperature = (roundTemperature((response.main.temp) * 9/5 + 32));
-              //verification, delete later if you see this.
-              console.log('Your current weather for your location is: ' + weatherTemperature);
-              
-              var userWeatherIcon = "<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='Icon depicting current weather.'>";
-              console.log('Your current weather icon code for your location: ' + response.weather[0].icon);
+                var weatherDescription = response.currently.summary;
+                //set our initial temperature to Fahrenheit due to the response data return the unit of celsius as the response data
+                var weatherTemperature = (roundTemperature((response.currently.temperature) * 9/5 + 32));
+                //verification, delete later if you see this.
+                console.log('The current weather for your location is: ' + weatherTemperature);
+                
+                var userWeatherIcon = response.currently[0].icon;
+                console.log('Your current weather icon code for your location: ' + userWeatherIcon);
+                
+                weatherData.temperatureValue = weatherTemperature;
+                /* took out weatherSimpleDescription in the loadBackground function parameter, replaced weatherDescription
+                   We dont want to set the icon to the actual string 'rain', but the 'rain' icon. 
+                   We should probably set our icons below this ajax call      */
+                //weatherData.icon.innerHTML = userWeatherIcon;
+                weatherData.city.innerHTML = cityName;
+                weatherData.temperature.innerHTML = weatherTemperature + weatherData.units;
+                weatherData.weather.innerHTML = weatherDescription;
+          }, false);
+    });        
 
-              var iconUrl = "http://openweathermap.org/img/w/" + userWeatherIcon + ".png";
-              $("#weatherIconSJC").html("<img src='" + iconUrl  + "'>");
-              $("#weatherIconLAX").html("<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png' alt='Icon depicting current weather.'>");
 
-              weatherData.temperatureValue = weatherTemperature;
-              // took out weatherSimpleDescription in the loadBackground function parameter, replaced weatherDescription
-              // loadBackground(position.latitude, position.longitude, weatherDescription);
-              // weatherData.icon.innerHTML = userWeatherIcon;
-              weatherData.city.innerHTML = cityName;
-              weatherData.temperature.innerHTML = weatherTemperature + weatherData.units;
-              weatherData.weather.innerHTML = weatherDescription;
-            }, false);
+/**
+ *  MIAMI SECTION, first attempt at trying to render multiple sections, means double the amount of API calls.
+ * will this reduce me rendering time? If so how do I increase the speed? 
+ */
 
-        })
-        
+    var weatherDataMIA = {
+      city: document.querySelector ("#cityMIA"),
+      state: document.querySelector ("#stateMIA"), 
+      weather: document.querySelector ("#weatherMIA"),
+      temperature: document.querySelector("#temperatureMIA"),
+      icon: document.querySelector ('#weatherIconMIA'),
+      temperatureValue: 0,
+      units: "°F"
+    };
+    // Might be able to refactor code to do this once with the 
+    // temperature for all ajax calls
+    function roundTemperature(temperature){
+          temperature = temperature.toFixed(1);
+          return temperature;
+    }
 
+    function switchUnits () {
+      if (weatherDataMIA.units == "°C") {
+        weatherDataMIA.temperatureValue = roundTemperature(weatherDataMIA.temperatureValue * 9/5 + 32);
+        weatherDataMIA.units = "°F";
+      
+      } else {
+        weatherDataMIA.temperatureValue = roundTemperature((weatherDataMIA.temperatureValue - 32) * 5/9);
+          weatherDataMIA.units = "°C";  
+      }
+        weatherDataMIA.temperature.innerHTML = weatherDataMIA.temperatureValue + weatherDataMIA.units + " ";
+    }
+
+    //https://api.darksky.net/forecast/845ff6b003aba8bd1a9434ab040ef79c/37.8267,-122.4233
+    // get geolocation data from ip via ip-api.com
+    var getIPMIA = 'http://ip-api.com/json/';
+    //Dark Sky API key: 845ff6b003aba8bd1a9434ab040ef79c
+    //OpenWeatherMap API key: 
+    // get weather data from Open Weather API, use geolocation 
+    // var openWeatherMap = 'http://api.openweathermap.org/data/2.5/weather'
+    // var darkSkyAPI = 'https://api.darksky.net/forecast/'
+    // var CORS = 'https://cors-anywhere.herokuapp.com/'
+    // var apiKey = '845ff6b003aba8bd1a9434ab040ef79c/'
+    $.getJSON(getIPMIA).done(function(locationMIA) {
+        //show location object in console, to pull out 'regionName' or the state for rendering in html
+        console.log(locationMIA);
+        console.log("Your region/state name is: " + locationMIA.regionName);
+          var regionNameMIA = locationMIA.regionName;
+          weatherDataMIA.state.innerHTML = regionName + ", ";
+          //Now that we have the location data we can pass this within our DarkSky Ajax call
+          var darkSkyURLforMIA = 'https://api.darksky.net/forecast/'
+          // To bypass the Cross Domain Error, can I pass in an object of mode: 'no-cors' as my proxy variable?
+          // or is this only used with the fetch function? 
+          // the use of:      
+          //        mode: 'no-cors' to get rid of the CORS issue: 
+          //    EXAMPLE:
+          //        fetch(apiLinkDS, { 
+          //              mode: 'no-cors' 
+          //           })
+          //
+          var corsProxyMIA = 'https://cors-anywhere.herokuapp.com/' //can I pass in a mode: 'no-cors' object in place of this proxy?
+          var apiKeyMIA = '845ff6b003aba8bd1a9434ab040ef79c/' 
+          // will locationData be a string? location.lat object + string + location.lat object == String??  
+          // OR is locationData = (response object + string object + response object) ??
+          var locationDataMIA = locationMIA.lat + "," + locationMIA.lon; //should I use ( ) to wrap these 3 objects together?
+          var requestObject = corsProxyMIA + darkSkyURLforMIA + apiKeyMIA + locationDataMIA;
+          //start Ajax call pass in the above data, in order:
+          // CORS + DarkSky API URL + API KEY + locationData
+          $.getJSON(requestObjectMIA).done(function(responseMIA) {
+                console.log(responseMIA);
+                //why do I need this positon object? 
+                var positionMIa = {
+                  latitude: responseMIA.latitude,
+                  longitude: responseMIA.longitude
+                };
+                var cityNameMIA = locationMIA.city;
+
+                var weatherDescription = response.currently.summary;
+                //set our initial temperature to Fahrenheit due to the response data return the unit of celsius as the response data
+                var weatherTemperature = (roundTemperature((response.currently.temperature) * 9/5 + 32));
+                //verification, delete later if you see this.
+                console.log('The current weather for your location is: ' + weatherTemperature);
+                
+                var userWeatherIcon = response.currently[0].icon;
+                console.log('Your current weather icon code for your location: ' + userWeatherIcon);
+                
+                weatherData.temperatureValue = weatherTemperature;
+                /* took out weatherSimpleDescription in the loadBackground function parameter, replaced weatherDescription
+                   We dont want to set the icon to the actual string 'rain', but the 'rain' icon. 
+                   We should probably set our icons below this ajax call      */
+                //weatherData.icon.innerHTML = userWeatherIcon;
+                weatherData.city.innerHTML = cityName;
+                weatherData.temperature.innerHTML = weatherTemperature + weatherData.units;
+                weatherData.weather.innerHTML = weatherDescription;
+          }, false);
+    });   
+
+// Instructions:
+// 1.) Comment out the weatherlyIcons.js script, then try to render this page w/ 
+// this weatherlyIcon.js code below uncommented. If error happens, log error below, and 
+//continue testing with this Skycons javascript below to figure out how to place this within same 
+// file. Will seperating Icon scripts from the actual scripts that completes the AJAX
+//call for the weatherData hinder each other? Log Updates below.
+// UPDATES: 
+// UPDATES:
+(function() {
+    if(!Skycons) {
+        //error, couldn't find location, execute sweetalert 'error' function
+        swal("Oops!", "Something went wrong on the page! Couldn't find your Skycons!", "error");
+    }  
+    else {
+         /* Set icons to the value of specific Skycons functions*/
+        var icons = new Skycons({"color": "white"});
+
+        icons.set("clear-day", Skycons.CLEAR_DAY); // User City
+        icons.set("clear-night", Skycons.CLEAR_NIGHT); 
+        icons.set("partly-cloudy-day", Skycons.PARTLY_CLOUDY_DAY);
+        icons.set("partly-cloudy-night", Skycons.PARTLY_CLOUDY_NIGHT);
+        icons.set("cloudy", Skycons.CLOUDY);    // Austin
+        icons.set("rain", Skycons.RAIN);    //Seattle
+        icons.set("sleet", Skycons.SLEET);  // Washington, D.C.
+        icons.set("snow", Skycons.SNOW);
+        icons.set("wind", Skycons.WIND);    
+        icons.set("fog", Skycons.FOG);   //Baltimore
+
+        // Icon Duplicates for weather
+        icons.set("clear-day2", Skycons.CLEAR_DAY); //San Jose
+        icons.set("clear-day3", Skycons.CLEAR_DAY);
+        icons.set("clear-day4", Skycons.CLEAR_DAY); //Denver
+
+        icons.set("rain2", Skycons.RAIN);   //Seattle
+        icons.set("fog2", Skycons.FOG);   //Baltimore 
+
+        icons.set("partly-cloudy-day2", Skycons.PARTLY_CLOUDY_DAY);  // Houston
+        icons.set("partly-cloudy-day3", Skycons.PARTLY_CLOUDY_DAY);  // Raleigh
+
+        icons.set("cloudy3", Skycons.CLOUDY); // Atlanta
+        icons.set("cloudy4", Skycons.CLOUDY); // Charlotte
+        icons.set("cloudy5", Skycons.CLOUDY); 
+
+        icons.play();
+
+    }
+})
 
 
 
