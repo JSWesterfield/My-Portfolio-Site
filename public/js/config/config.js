1e8abb024794ec4a38eea6814726dbf3e39f1e28
Jacob Westerfield <Jacob_Westerfield@discovery.com>
2020-03-06 00:19:55 -0500

    installs config library, npm i config. Creates a config folder at the root of directory for all applications to retrieve from. Places in dev.json the old config.js data

--- public/js/config/config.js
+++ public/js/config/config.js
@@ -1,8 +1,5 @@
 var config = {
-    //Open Weather Map API key
-    owm_KEY: "d8be0f02752ec0698888d899e60425d8",
-    //Flickr API key
-    flickr_APIKey: "dasdasd2213",
-    //Dark Sky API key
-    darkSky: "845ff6b003aba8bd1a9434ab040ef79c"
+    owm_KEY: "d8be0f02752ec0698888d899e60425d8",  //Open Weather Map API key
+    flickr_APIKey: "dasdasd2213", //Flickr API key
+    darkSky: "845ff6b003aba8bd1a9434ab040ef79c" //Dark Sky API key
 }
